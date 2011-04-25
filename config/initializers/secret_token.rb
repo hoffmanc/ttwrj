ea4a76b9a4575ade4145a0e48c3b371eebcf73c7
Chris Hoffman <chris.c.hoffman@gmail.com>
2011-04-25 07:07:55 -0400

    initial rails 3 init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ttwrj::Application.config.secret_token = '574ebdb054cbd65b3a3be38d64b0432e82e092a9121856a05de89274cd540785fd98e77c41c09be52d517d0888018e8789272b29b081cb22092716b28645cbd8'
